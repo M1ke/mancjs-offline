43acc085e4c538689fdd3dd703b0707d8546e46e
m1ke <git@m1ke.me>
2013-08-07 17:18:23 +0100

    better ambiguous name/gender searching

--- assets/node-gender.js
+++ assets/node-gender.js
@@ -8,7 +8,7 @@ M1ke
 
 */
 
-var genderGuesser = {
+var genderGuesser={
 	genders:{},
 	gotGenders:false,
 	find: function(name) {
@@ -16,21 +16,36 @@ var genderGuesser = {
 			return;
 		}
 		if (this.gotGenders) {
-			this.findName(name,this.genders);
+			this.findName(name,this.genders,this.names);
 		}
 		else {
 			this.getGenders(name);
 		}
 	},
-	findName: function(name,genders) {
-		var gender=genders[name.toUpperCase()] || 'male';
+	findName: function(nameInput,genders) {
+		var gender;
+		nameInput=nameInput.toUpperCase();
+		if (genders[nameInput]) {
+			gender=genders[nameInput];
+		}
+		else {
+			for (var name in genders) {
+				if (name.match(nameInput)) {
+					gender=genders[name];
+					break;
+				}
+			}
+			if (!gender) {
+				gender='ambiguous';
+			}
+		}
 		$('div.alert strong').text(gender).parent().fadeIn();
 	},
 	getGenders: function(name) {
-		var self = this;
+		var self=this;
 		$.getJSON('genders.json',function(response){
-			self.genders = response;
-			self.gotGenders = true;
+			self.genders=response;
+			self.gotGenders=true;
 			self.findName(name,response);
 		});
 	},
